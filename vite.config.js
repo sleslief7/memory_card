e15cb00c1b7cc37f61616cd1b429fdb41defe15f
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-11-15 11:51:05 -0500

    deploying fix

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.emoji.family/api',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
